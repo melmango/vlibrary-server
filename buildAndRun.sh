49746e86d0d3263be3a1b8b94c9e11b4cab0328f
Melman <melmanguo@gmail.com>
2016-07-03 21:21:53 +0800

    Change framework from Gin to Beego, for easier RESTful support

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,3 +1,11 @@
-go clean
-go build
-./vlibrary-server
+#!/usr/bin/env bash
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build main.go
+rm -f vlibrary-linux.tar.gz
+rm -rf vlibrary-linux
+mkdir vlibrary-linux
+cp main vlibrary-linux/cintool
+cp -R views vlibrary-linux/
+cp -R static vlibrary-linux/
+cp -R conf vlibrary-linux/
+tar -zcf vlibrary-linux.tar.gz vlibrary-linux
+rm -rf vlibrary-linux
---
+++ models/models.go
@@ -0,0 +1,13 @@
+package models
+
+type VUser struct{
+	
+}
+
+type VPage struct{
+
+}
+
+type VUrl struct{
+
+}
